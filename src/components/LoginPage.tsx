d54b7238a6772d223a3ea4e8c958420a034329d0
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-08 23:37:17 +0100

    updated files

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BsWhatsapp, BsTwitter, BsGithub } from "react-icons/bs";
 import { FaGoogle } from "react-icons/fa";
 import { GrFacebook } from "react-icons/gr";
@@ -9,11 +9,18 @@ import "./LoginPage.css";
 
 const LoginPage = (): JSX.Element => {
   const [isLight, setIsLight] = useState(true);
-
+  const focusPoint = useRef<HTMLInputElement>(null);
   const setLightMode = () => {
     setIsLight(!isLight);
     return document.body.classList.toggle("dark-mode");
   };
+
+  useEffect(() => {
+    if (focusPoint.current) {
+      focusPoint.current.focus();
+      return;
+    }
+  }, [focusPoint]);
 
   return (
     <div className="box-container">
@@ -31,7 +38,7 @@ const LoginPage = (): JSX.Element => {
       <div className="form">
         <div className="format-box">
           <MdEmail className="email-icon" />
-          <input type="text" placeholder="Email" />
+          <input type="text" ref={focusPoint} placeholder="Email" />
         </div>
         <div className="format-box">
           <IoIosLock className="lock-pass" />
